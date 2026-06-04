4c5750ec2e203730c8acf29b39ee4fc3d834b23d
JPeaceOA <jpeaceoa@gmail.com>
2026-06-04 13:46:44 +0100

    added 4 pages: admin dash, customer view, payment pending and business create

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+import Hero from '@/components/Hero';
+import HowItWorks from '@/components/HowItWorks';
+import Temp from '@/components/temp';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <HowItWorks />
+      <Temp />
+    </>
+  );
+}
